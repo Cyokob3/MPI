db0c06cc931e14d0544b696f14fcf3db05da6cc6
Cyokob3 <c6102010@planet.kanazawa-it.ac.jp>
2021-06-23 11:34:44 +0900

    ゴールドバッチの実装完了

--- goldbachMPI.c
+++ goldbachMPI.c
@@ -1,6 +1,19 @@
+// ゴールドバッチ
 #include "mpi.h"
 #include "stdio.h"
 #include "stdlib.h"
+
+int is_prime(n){
+    int i;
+    if(n < 2) return 0;
+    for(i=2; i<n; i++){
+        if(n % i == 0){
+            return 0;
+        }
+    }
+    return 1;
+}
+
 
 int main( argc, argv )
 int argc;
@@ -8,6 +21,8 @@ char **argv;
 {
   int myrank;
   double start, time;
+  int i, a;
+  char flag;
   int n = 3;
   MPI_Status status;
   MPI_Init( &argc, &argv );
@@ -15,8 +30,24 @@ char **argv;
   start = MPI_Wtime();
   printf("Hello from %d node\n",myrank);
   while(n <= 100){ 
+    // 偶数であるかの判定を行う
+    flag = 0;
+    if(!(n % 2 == 0)){
+      n++;
+      continue;
+    }
     printf("%d : ", n);
-    printf("ok\n"); 
+    for (i=2; i<n; i++){
+      if(!(is_prime(i))) continue;
+      a = n - i;
+      if(!(is_prime(a))) continue;
+      flag = 1;
+    }
+    if (flag == 1) printf("ok\n");
+    else {
+      printf("error\n");
+      break;
+    }
     n++;
   }
   time=MPI_Wtime()-start;
